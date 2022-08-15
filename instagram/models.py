4a4e36dcaef409ca9308fd986e627e0ac4c1bacf
inshining <inshining.kim@gmail.com>
2022-08-15 15:14:05 +0900

    기본 viewset 테스트

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    message = models.CharField(max_length= 200, verbose_name="메세지")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- instagram/urls.py
+++ instagram/urls.py
@@ -1,8 +1,13 @@
-from django.urls import path
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+
+router = DefaultRouter()
+
+router.register('post', views.PostViewSet ) #2개 패턴으로 줌
 
 app_name = 'instagram'
 
 urlpatterns = [
+    path('', include(router.urls))
 ]
